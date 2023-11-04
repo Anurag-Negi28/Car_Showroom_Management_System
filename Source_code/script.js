89ee181a816b8ee6aab1e430f6d9eece5ea96b7a
Madhvendra Singh <chouhanmadhvendra13@gmail.com>
2023-11-04 16:22:38 +0530

    added logo

--- Source_code/script.js
+++ Source_code/script.js
@@ -78,5 +78,42 @@ document.addEventListener("DOMContentLoaded", function() {
         // When the video ends, hide page 1 and display page 2
         page1.style.display = "none";
         page2.style.display = "block";
+        })
     });
+        const logo = document.getElementById("logo-container");
+            // Define the scroll event listener
+    window.addEventListener("scroll", function() {
+        // Calculate the opacity based on the scroll position
+        const scrollY = window.scrollY;
+        const maxScroll = 10; // Adjust this value to control the scroll range for opacity change
+        const opacity = 1 - Math.min(1, scrollY / maxScroll);
+
+        // Update the logo's opacity
+        logo.style.opacity = opacity;
+
+    logo.addEventListener("mouseenter", function() {
+        // Add a hover effect (e.g., scaling the logo)
+        logo.style.transform = "scale(1.1)";
+        // You can also add other hover effects like changing opacity or background color
+    });
+
+    logo.addEventListener("mouseleave", function() {
+        // Remove the hover effect (e.g., scale back to the original size)
+        logo.style.transform = "scale(1)";
+    });
+        const logoLink = document.getElementById("logo-link");
+
+    logoLink.addEventListener("click", function(event) {
+        // Prevent the default behavior of the anchor element (e.g., navigating to a URL)
+        event.preventDefault();
+
+        // Define the URL you want to navigate to when the logo is clicked
+        const targetURL = "index.html"; // Replace with your desired URL
+
+        // Use JavaScript to navigate to the target URL
+        window.location.href = targetURL;
+
+    });
+
+
 });
