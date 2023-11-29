39b193877e2d58686da46b4d3e1e0c3c7c2f1890
Anurag Negi <123005508+Anurag-Negi28@users.noreply.github.com>
2023-11-30 02:20:25 +0530

    Tiitla added

--- Backend/listings/admin.py
+++ Backend/listings/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Listing
 # Register your models here.
 
+admin.site.site_header = "Deals on Wheels Admin"
+admin.site.site_title = "Deals on Wheels Admin Portal"
+admin.site.index_title = "Welcome to Deals on Wheels Portal"
+
 admin.site.register(Listing)
