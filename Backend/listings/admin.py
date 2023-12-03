87affcd8995f08ab1b87f357bd6c2225d4e1a99b
Anurag Negi <123005508+Anurag-Negi28@users.noreply.github.com>
2023-12-03 19:24:01 +0530

    Page linked with index

--- Backend/listings/admin.py
+++ Backend/listings/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Listing
+from .models import Customer
+from .models import Mechanic
+from .models import Attendance
+from .models import Contact
 # Register your models here.
 
 admin.site.site_header = "Deals on Wheels Admin"
@@ -7,3 +11,7 @@ admin.site.site_title = "Deals on Wheels Admin Portal"
 admin.site.index_title = "Welcome to Deals on Wheels Portal"
 
 admin.site.register(Listing)
+admin.site.register(Customer)
+admin.site.register(Mechanic)
+admin.site.register(Attendance)
+admin.site.register(Contact)
--- Backend/listings/models.py
+++ Backend/listings/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here
 
@@ -10,5 +11,44 @@ class Listing(models.Model):
     price = models.IntegerField()
     image = models.ImageField()
 
+class Customer(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20,null=False)
+    @property
+    def get_name(self):
+        return self.user.first_name+" "+self.user.last_name
+    @property
+    def get_instance(self):
+        return self
     def __str__(self):
-        return self.name 
+        return self.user.first_name
+    
+class Mechanic(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20,null=False)
+    skill = models.CharField(max_length=500,null=True)
+    salary=models.PositiveIntegerField(null=True)
+    status=models.BooleanField(default=False)
+    @property
+    def get_name(self):
+        return self.user.first_name+" "+self.user.last_name
+    @property
+    def get_id(self):
+        return self.user.id
+    def __str__(self):
+        return self.user.first_name
+    
+class Attendance(models.Model):
+    mechanic=models.ForeignKey('Mechanic',on_delete=models.CASCADE,null=True)
+    date=models.DateField()
+    present_status = models.CharField(max_length=10)
+
+class Contact(models.Model):
+    date=models.DateField(auto_now=True)
+    by=models.CharField(max_length=40)
+    message=models.CharField(max_length=500)
+    
+def __str__(self):
+    return self.name 
